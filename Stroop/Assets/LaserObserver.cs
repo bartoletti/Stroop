2039abd2c01c9bbb78f5f72c07cc9e08a1965fb9
bartoletti <nick_bartoletti@yahoo.com>
2018-08-01 11:22:57 -0700

    Initial commit

---
+++ Stroop/Assets/LaserObserver.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserObserver : MonoBehaviour {
+
+    public GameObject other;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        Animator anim = this.GetComponent<Animator>();
+        anim.SetBool("isTargeted", true);
+        Debug.Log("Enter");
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        Animator anim = this.GetComponent<Animator>();
+        anim.SetBool("isTargeted", false);
+        Debug.Log("Exit");
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
